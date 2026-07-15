6c6bf648ec420c69791f625132f767e17eb8ae19
Alexander Gutkin <agutkin@google.com>
2026-07-15 21:00:56 +0000

    Add unit test for Minimize in Thrax standard library.
    PiperOrigin-RevId: 948531807

---
+++ opengrm/thrax/walker/util/function/minimize_test.cc
@@ -0,0 +1,69 @@
+// Copyright 2026 The OpenGrm Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "opengrm/thrax/walker/util/function/minimize.h"
+
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "gtest/gtest.h"
+#include "openfst/lib/arc.h"
+#include "openfst/lib/equal.h"
+#include "openfst/lib/fst.h"
+#include "openfst/lib/minimize.h"
+#include "openfst/lib/vector-fst.h"
+#include "opengrm/thrax/walker/util/datatype.h"
+
+namespace thrax {
+namespace function {
+
+template <typename Arc>
+class MinimizeTest : public ::testing::Test {
+ protected:
+  using Transducer = ::fst::Fst<Arc>;
+  using MutableTransducer = ::fst::VectorFst<Arc>;
+
+  Minimize<Arc> func_;
+};
+
+using ArcTypes = ::testing::Types<::fst::StdArc, ::fst::LogArc>;
+TYPED_TEST_SUITE(MinimizeTest, ArcTypes, );
+
+// Tests that the Minimize function correctly minimizes a deterministic FST
+// by merging equivalent states.
+TYPED_TEST(MinimizeTest, TestMinimize) {
+  auto input = std::make_unique<typename TestFixture::MutableTransducer>();
+  auto p = input->AddState();
+  input->SetStart(p);
+  auto q1 = input->AddState();
+  auto q2 = input->AddState();
+  input->EmplaceArc(p, 'a', 'a', q1);
+  input->EmplaceArc(p, 'b', 'b', q2);
+  input->SetFinal(q1);
+  input->SetFinal(q2);
+
+  auto expected = *input;
+  ::fst::Minimize(&expected);
+
+  auto args = std::make_unique<std::vector<std::unique_ptr<DataType>>>(1);
+  (*args)[0] = std::make_unique<DataType>(std::move(input));
+  auto result_data = this->func_.Run(std::move(args));
+  auto* result =
+      *result_data->template get<typename TestFixture::Transducer*>();
+  EXPECT_TRUE(::fst::Equal(*result, expected));
+}
+
+}  // namespace function
+}  // namespace thrax
