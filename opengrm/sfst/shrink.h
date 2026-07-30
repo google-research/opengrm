d1b5cd7742356abdeae07b86c2053538f102d7ef
Kyle Gorman <kbg@google.com>
2026-07-30 17:42:15 +0000

    Fix sfst::StolckeShrink by adding optional Filter predicate support and adding normalization.
    PiperOrigin-RevId: 956612227

--- opengrm/sfst/shrink.h
+++ opengrm/sfst/shrink.h
@@ -19,6 +19,7 @@
 
 #include <algorithm>  // NOLINT(misc-include-cleaner)
 #include <cmath>
+#include <cstddef>
 #include <fstream>
 #include <set>  // NOLINT(misc-include-cleaner)
 #include <sstream>
@@ -35,6 +36,7 @@
 #include "openfst/lib/mutable-fst.h"
 #include "openfst/lib/symbol-table.h"
 #include "opengrm/sfst/canonical.h"
+#include "opengrm/sfst/normalize.h"
 #include "opengrm/sfst/sfst.h"
 
 namespace sfst {
@@ -127,9 +129,12 @@ void ComputeStateProbs(const fst::ExpandedFst<Arc>& fst,
 // transitions. General SFST models containing loops or back-edges between
 // the same or lower orders are not actively supported for forward probability
 // mass.
-template <class Arc>
+// An optional `filter` callable `bool(StateId s, Label l)` can be passed to
+// skip pruning specific arcs (returning true prevents pruning). By default,
+// filter is nullptr_t and no filtering is applied.
+template <class Arc, class Filter = std::nullptr_t>
 bool StolckeShrink(fst::MutableFst<Arc>* fst, typename Arc::Label phi_label,
-                   double theta) {
+                   double theta, Filter filter = nullptr) {
   if (!IsCanonical(*fst, phi_label)) {
     LOG(ERROR) << "StolckeShrink: input is not a canonical SFST";
     return false;
@@ -211,6 +216,9 @@ bool StolckeShrink(fst::MutableFst<Arc>* fst, typename Arc::Label phi_label,
         score += secondterm;
         score *= -std::exp(log_prob_s);
         if (score <= log_theta) {
+          if constexpr (!std::is_same_v<Filter, std::nullptr_t>) {
+            if (filter(s, arc.ilabel)) continue;
+          }
           to_prune.push_back({s, arc.ilabel});
         }
       }
@@ -232,10 +240,11 @@ bool StolckeShrink(fst::MutableFst<Arc>* fst, typename Arc::Label phi_label,
       fst->AddArc(s, arc);
     }
   }
+  PhiNormalize(fst, phi_label);
   return true;
 }
 
-// Seymore and Rosenfeld style model shrinking.
+// Seymore and Rosenfeld model shrinking.
 //
 // Computes shrink score for transition based on Seymore/Rosenfeld formula:
 // N(w,h) [ log p(w|h) - log p'(w|h) ] where N(w,h) is discounted frequency.
